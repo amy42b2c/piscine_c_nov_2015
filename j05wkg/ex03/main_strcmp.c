b8ba79c2d5eb78d74581a771986bbdd662ff541a
Amy CRESAP <acresap@e3r12p10.42.fr>
2016-01-15 13:57:31 +0100

    j05, ex03

--- j05wkg/ex03/main_strcmp.c
+++ j05wkg/ex03/main_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: acresap <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/15 08:59:04 by acresap           #+#    #+#             */
-/*   Updated: 2016/01/15 12:50:52 by acresap          ###   ########.fr       */
+/*   Updated: 2016/01/15 13:50:28 by acresap          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,31 @@ int	main(void)
 	char	*string2;
 	char	*string3;
 	char	*string4;
-	int		l;
 
 	string1 = "My example sentence is really lame.";
-	string2 = "My exemple sentence is really lame.";
-	l = 0;
-	printf("Length of string1 is         %d.\n", ft_strlen(string1));
-	printf("Length of string2 is         %d.\n", ft_strlen(string2));
-//	printf("Length of string2 is %d.\n", ft_strlen("L'ecole c'est geniale"));
+	string2 = "My example sentence is really lame.";
+	string3 = "My example sentence is really lame";
+	string4 = "My exemple sentence is really lame.";
 
-	printf("String1 is [%s].\n", string1);
-	printf("String2 is [%s].\n", string2);
+	printf("\n");
+	/* print the strings */
+	printf("Original String1 is [%s].\n", string1);
+	printf("	 String2 is [%s].\n", string2);
+	printf("	 String3 is [%s].\n", string3);
+	printf("	 String4 is [%s].\n", string4);
+	printf("\n");
 
+	/* Test of my ft_strcmp function to compare to the real one: */
+	printf("Retour str2 ma ft_strcmp:   [%d].\n", ft_strcmp(string1, string2));
 	/* Test the REAL function "strcmp" to understand what it returns: */
 	printf("Retour la valeur de strcmp: [%d].\n", strcmp(string1, string2));
+	printf("\n");
+	
+	printf("Retour str3 ma ft_strcmp:   [%d].\n", ft_strcmp(string1, string3));
 	printf("Retour la valeur de strcmp: [%d].\n", strcmp(string1, string3));
+	printf("\n");
+	
+	printf("Retour str4 ma ft_strcmp:   [%d].\n", ft_strcmp(string1, string4));
 	printf("Retour la valeur de strcmp: [%d].\n", strcmp(string1, string4));
-	/* Test of my ft_strcmp function to compare to the real one: */
-	printf("Retour de   ma ft_strcmp:   [%d].\n", ft_strcmp(string1, string2));
+	printf("\n");
 }
